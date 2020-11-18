a25309bfd4216604c5ace20af4774b83564cb7c2
Hobart Phillips <melonkony@icloud.com>
2020-11-18 22:40:34 +1100

    Updated typography and header styles

--- en.search.min.04051c490b294a5b9a9c17ad35d396e05f66c33168e799bf63a4440aef8f27a6.js
+++ en.search.min.04051c490b294a5b9a9c17ad35d396e05f66c33168e799bf63a4440aef8f27a6.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/fr.search-data.min.92cb512495eeeb24f0abd2ae625ad82be00188725b3bc30c8b1d5341a17aec80.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.96e34b83f69e649791dd937266bf6b9d28b76dbb854e14be0be43fc760bf6eed.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
