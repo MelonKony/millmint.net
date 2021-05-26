6471c1ec2baff0c5f1f48296e65a30f966504879
MelonKony <melonkony@icloud.com>
2021-05-26 20:08:53 +1000

    Updated typography/styling

--- js/nav.js
+++ js/nav.js
@@ -1,13 +1,39 @@
-var $dot = $('.dot');
+// toc autoscroll //
 
-$('ul.breadcrumb li').on('mouseover', function(){
-  $dot.css({
-    left: $(this).offset().left
-    - $(this).parent().offset().left
-    + $(this).width() / 5 - 10
-  });
+$(document).ready(function() {
+  $(window).scroll(function() {
+    $(".book-toc a").removeClass("current")
+    currentAnchor().addClass("current")
+  })
 });
 
-$('ul.breadcrumb li').on('mouseout', function(){
-  $('.dot').css({left: 185});
-});
+function tocItem(anchor) {
+  return $("[href=\"" + anchor + "\"]")
+}
+
+function heading(anchor) {
+  return $("[id=" + anchor.substr(1) + "]")
+}
+
+var _anchors = null
+function anchors() {
+  if (!_anchors) {
+    _anchors = $(".book-toc a").map(function() {
+      return $(this).attr("href")
+    })
+  }
+  return _anchors
+}
+
+function currentAnchor() {
+  var winY = window.pageYOffset
+  var currAnchor = null
+  anchors().each(function() {
+    var y = heading(this).position().top
+    if (y < winY + window.innerHeight * 0.23) {
+      currAnchor = this
+      return
+    }
+  })
+  return tocItem(currAnchor)
+}
