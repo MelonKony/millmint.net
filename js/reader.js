38aa145d249f48c1a6b3fcd89909a7d3473fa158
vttasmania <melonkony@icloud.com>
2021-05-27 14:13:03 +1000

    Added umami analytics

--- js/reader.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	  $(".toggle").click(function() {
-		$(".content").toggle();
-		$(this).text(function(i, t) {
-		  return t == 'Show' ? 'Hide' : 'Show';
-		})
-	  });
-	});
